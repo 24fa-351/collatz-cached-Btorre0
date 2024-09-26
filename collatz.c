6f2f6409a158abbe394cd4eb01231b1fc4b08d36
Beatriz Torres <btorre0@csu.fullerton.edu>
2024-09-26 13:49:34 -0700

    set up files + understand assignment

---
+++ collatz.c
@@ -0,0 +1,21 @@
+// basic Collatz explaination:
+// If it's even, divide by two, if it's odd, multiply by three and add one.
+// Repeat step 2 indefinitely.
+// The conjecture states that you will always reach the number one eventually.
+// For example, say you start with 5 your sequence would go 5-16-8-4-2-1.
+
+
+// if n != 0 {
+// while num = odd
+// (N /=2)
+// while num  = even {
+// n = (n * 3) + 1}
+//
+//
+// keep this repeating until n = 0
+
+// Requirements:
+
+// count steps until you get to one
+
+//
