84b25df0eb631289808a4f9329e6bfd8a08a8baf
Beatriz Torres <btorre0@csu.fullerton.edu>
2024-09-27 12:46:01 -0700

    getting started

--- collatz.c
+++ collatz.c
@@ -1,21 +1,63 @@
-// basic Collatz explaination:
-// If it's even, divide by two, if it's odd, multiply by three and add one.
-// Repeat step 2 indefinitely.
-// The conjecture states that you will always reach the number one eventually.
-// For example, say you start with 5 your sequence would go 5-16-8-4-2-1.
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
 
 
-// if n != 0 {
-// while num = odd
-// (N /=2)
-// while num  = even {
-// n = (n * 3) + 1}
-//
-//
-// keep this repeating until n = 0
+// place holders
+#define MIN_VALUE 1
+#define MAX_VALUE 500000
 
-// Requirements:
+// steps in collatz (and the process itself)
+unsigned long long int collatz_function (unsigned long int n) {
+    int numberOfSteps = 0;
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else if (n % 2 == 1) {
+            n = (n * 3) + 1;
+        }
+        numberOfSteps++;
+    }
+    return numberOfSteps;
+}
 
-// count steps until you get to one
+int randomNumber(int min, int max) {
+    int randomNum = rand() % ((max - min) + 1) + min;
+    return randomNum;
+}
 
-//
+// LRU
+
+
+
+typedef struct cache {
+
+} Cache;
+
+typedef struct Nodes {
+    struct Nodes *prev, *next;
+
+} Nodes;
+
+// running the collatz
+// open file (testing)
+// write a for loop that uses functions and structs
+// close file
+
+
+
+int main( int argc , char *argv[] ) {
+    // will later initialize
+    int N;
+    int MIN;
+    int MAX;
+    // 
+    if (argc != 4) {
+
+    }
+
+
+
+    return 0;
+}
