d96fb5f43fde2d01012ac67f92a51d818567a61e
Beatriz Torres <btorre0@csu.fullerton.edu>
2024-09-28 12:34:02 -0700

    non-caching complete

--- collatz.c
+++ collatz.c
@@ -18,52 +18,67 @@ unsigned long long int collatz_function (unsigned long long int n) {
     return numberOfSteps;
 }
 
-int randomNumber(int min, int max) {
-    int randomNum = rand() % ((max - min) + 1) + min;
-    return randomNum;
+// random number
+unsigned long long int RN(int min, unsigned long long int max) {
+    int RN = rand() % ((max - min) + 1) + min;
+    return RN;
 }
 
-// LRU
+// instead of using classes- i'll be trying out structs!
+// caching layer started
+// typedef struct Nodes {
+//     unsigned long long int key;
+//     unsigned long long int valueSteps;
+//     struct Nodes *prev, *next;
 
-typedef struct Nodes {
-    unsigned long long int key;
-    unsigned long long int valueSteps;
-    struct Nodes *prev, *next;
+// } Nodes;
 
-} Nodes;
+// typedef struct cache {
+//     int capacity;
+//     int size;
 
-typedef struct cache {
-    int capacity;
-    int size;
+//     Nodes *head, *tail;
 
-    Nodes *head, *tail;
-    Nodes **hashtable; 
+// } Cache;
 
-} Cache;
-
-
-// running the collatz
-// open file (testing)
-// write a for loop that uses functions and structs
-// close file
-
-
-
+// first complete the non caching layer:
 int main( int argc , char *argv[] ) {
-    
+
+    if (argc != 4) {
+        printf("Usage: %s <N> <MIN> <MAX>\n", argv[0]);
+        return 1;
+    }
+
     int N = atoi(argv[1]);
     int MIN = atoi(argv[2]);
     int MAX = atoi(argv[3]);
 
-    if (argc != 4) {
-      printf("Usage: %s <N> <MIN> <MAX>\n", argv[0]);
-      return 1;
-    }
-    if (MIN < 1 || MAX > 500000) {
+    // for testing use
+    if (MIN < 1 || MIN > MAX) {
         printf("Invalid range\n");
         return 1;
     }
-    // logic cont.
 
+    srand(time(0));
+
+    FILE *file = fopen("collatz.csv", "w");
+
+    if (!file) {
+        printf("Could not open file\n");
+        return 1;
+    }
+
+    for (int ix = 0; ix < N; ix++) {
+
+        unsigned long long int num = RN(MIN, MAX);
+
+        unsigned long long steps = collatz_function(num);
+        
+        // in file: ( random number, number of steps )
+        fprintf(file, "%llu,%llu\n", num, steps);
+    }
+
+    fclose(file);
     return 0;
+    
 }
